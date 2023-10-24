be9e85adb3e50cb2ee5eaf5b717cf293db664391
n.m.borisova <K8sedrf@1q>
2023-10-24 15:45:48 +0300

    Added Product entity

---
+++ src/bg/smg/model/Product.java
@@ -0,0 +1,73 @@
+package bg.smg.model;
+
+import java.sql.Timestamp;
+
+public class Product {
+    private int id;
+    private String name;
+    private String description;
+    private Category category;
+    private int unit_price;
+    private Timestamp created;
+    private boolean is_active;
+
+    Product() {
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+
+    public int getUnit_price() {
+        return unit_price;
+    }
+
+    public void setUnit_price(int unit_price) {
+        this.unit_price = unit_price;
+    }
+
+    public Timestamp getCreated() {
+        return created;
+    }
+
+    public void setCreated(Timestamp created) {
+        this.created = created;
+    }
+
+    public boolean isIs_active() {
+        return is_active;
+    }
+
+    public void setIs_active(boolean is_active) {
+        this.is_active = is_active;
+    }
+}
